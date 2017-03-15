e77f39383e33f48b99643a26a390c7778e40ed1f
Stephan Sum <sum@live.de>
2017-03-15 11:16:07 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import "./custom.scss";
+
+import $ from "jquery";
+window.$ = window.jQuery = $;  // make jQuery globally available
+
+
+$(() => {
+    
+    
+
+});
