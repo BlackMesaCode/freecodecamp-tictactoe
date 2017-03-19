ebb94818eafc728e64678ae8862f1015bdc5f7b5
Stephan Sum <sum@live.de>
2017-03-19 10:34:19 +0100

    added mode selection

---
+++ src/components/SelectMode.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export default class SelectMode extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: "1p"
+        }
+    }
+
+    changeMode(event) {
+        this.setState({
+            mode: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Please select a game mode:</h3>
+                <div className="radio-buttons">
+                    <label>
+                        <input type="radio" name="mode" value="1p" onChange={this.changeMode.bind(this)} checked={this.state.mode === "1p"} />
+                        <span>1 Player</span>
+                    </label>
+                    <label>
+                        <input type="radio" name="mode" value="2p" onChange={this.changeMode.bind(this)} checked={this.state.mode === "2p"} />
+                        <span>2 Players</span>
+                    </label>
+                </div>
+                <Link to={`/SelectMark/${this.state.mode}`}>Next</Link>
+            </div>
+        );
+    }
+}
